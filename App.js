746eac5cf33d587123715415f0309d3d66b09ba7
lucasbalderrama <abreulucas3715@gmail.com>
2025-05-15 11:04:27 -0300

    app.js

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Flavia Glenda N°04 e Lucas Randal N°18
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import login from './src/screens/login.js';
+import cadastro from './src/screens/cadastro.js';
+import listarImg from './src/screens/listarImg.js';
+import listarVideo from './src/screens/listarVideo.js';
+import uploadImg from './src/screens/uploadImg.js';
+import uploadVideo from './src/screens/uploadVideo.js';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Stack = createNativeStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => (
+  <NavigationContainer>
+    -<Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>
+      <Stack.Screen name="login" component={login} />
+      <Stack.Screen name="cadastro" component={cadastro} />
+      <Stack.Screen name="listarImg" component={listarImg} />
+      <Stack.Screen name="listarVideo" component={listarVideo} />
+      <Stack.Screen name="uploadImg" component={uploadImg} />
+      <Stack.Screen name="uploadVideo" component={uploadVideo} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+
+export default App;
